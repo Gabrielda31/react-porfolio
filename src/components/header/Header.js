94a7897d2843eb9fd1225a616d94cda237d1c787
Gabrielda31 <gabrielda3108@gmail.com>
2020-08-02 22:25:30 +0700

    Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
 
 function Header() {
   return (
-    <section className="header-container"></section>
+    <section className="header__wrapper">
+      <div className="header__mainTitle">
+        <h3 className="animate__bounceInLeft">Dat Do</h3>
+        <p>Software Developer</p>
+      </div>
+    </section>
   );
 }
 
