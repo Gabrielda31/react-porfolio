40c7680757f7507f6830f912c9cfe39d13c151c8
Gabriel Do <gabrielda3108@gmail.com>
2020-08-05 18:43:14 +0700

    Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,8 +5,14 @@ function Header() {
   return (
     <section className="header__wrapper">
       <div className="header__mainTitle">
-        <h3 className="animate__bounceInLeft">Dat Do</h3>
+        <img src="" alt="" />
+        <h3 className="">Dat Do</h3>
         <p>Software Developer</p>
+      </div>
+      <div className="header__moreBtn">
+        <div className="header__square1"></div>
+        <div className="header__square2"></div>
+        <div className="header__square3 "></div>
       </div>
     </section>
   );
